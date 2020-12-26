5bbc559aa361664d8fa87a13b2cff0312a9e0936
kmachida12345 <kmachida12345@gmail.com>
2020-12-26 16:15:14 +0900

    Initial commit

---
+++ lib/HomeViewModel.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class HomeViewModel extends ChangeNotifier {
+  int _counter = 0;
+  int get counter => _counter;
+
+  void incrementCounter() {
+    this._counter++;
+    notifyListeners();
+  }
+}
