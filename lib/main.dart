ee7ef9833e2c349a80d3305fe8a6644e4c815a6a
machida.kohei <kohei.machida@partners.toshiba-visual.com>
2021-01-07 15:40:24 +0900

    画面遷移処理を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:todoapp/HomeViewModel.dart';
 
@@ -7,34 +8,51 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
+      routes: <String, WidgetBuilder>{
+        '/home': (BuildContext context) => new HomePage(),
+        '/subpage': (BuildContext context) => new ProviderPractice()
+      },
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              child: const Text('using Provider'),
+              color: Colors.orange,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.of(context).pushNamed('/subpage');
+              },
+            ),
+            RaisedButton(
+              child: const Text('using Riverpod'),
+              color: Colors.orange,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.of(context).pushNamed('/subpage');
+              },
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class ProviderPractice extends StatelessWidget {
+  ProviderPractice({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
