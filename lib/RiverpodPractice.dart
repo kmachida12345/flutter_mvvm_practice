da83c9ecbe73122ab689c93cb37269d3fc520402
kmachida12345 <kmachida12345@gmail.com>
2021-01-11 23:39:14 +0900

    firebase連携 データ追加

--- lib/RiverpodPractice.dart
+++ lib/RiverpodPractice.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,8 +10,26 @@ final counterProvider =
 
 // Extend ConsumerWidget instead of StatelessWidget, which is exposed by Riverpod
 class RiverpodPractice extends ConsumerWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context, ScopedReader watch) {
+
+    Future<void> addUser() {
+      CollectionReference users =
+      FirebaseFirestore.instance.collection('testdesu');
+
+      return users
+          .add({
+        'hoge': 'hoge ${context.read(counterProvider).counter}',
+        'fuga': 'fuga',
+        'age': 1
+      })
+          .then((value) => print('hoge user added.'))
+          .catchError((error) => print('hoge failed. $error'));
+    }
+
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Example')),
@@ -19,28 +38,48 @@ class RiverpodPractice extends ConsumerWidget {
           children: [
             Text('${watch(counterProvider).counter}'),
             RaisedButton(
-              child: const Text('add data'),
+              child: Text('add data'),
               color: Colors.orange,
               textColor: Colors.white,
-              onPressed: () {
-                print('hoge');
-                CollectionReference users =
-                    FirebaseFirestore.instance.collection('addtestdesu');
-                users
-                    .add({
-                      'hoge': 'hoge${watch(counterProvider).counter}',
-                      'fuga': 'fuga',
-                      'age': 1
-                    })
-                    .then((value) => print('hoge user added.'))
-                    .catchError((error) => print('hoge failed. $error'));
-              },
+              onPressed: addUser,
             )
           ],
         ),
         floatingActionButton: _RiverpodPracticePageFab(),
       ),
     );
+
+
+    // return FutureBuilder(
+    //     future: _initialization,
+    //     builder: (context, snapshot) {
+    //       if (snapshot.connectionState == ConnectionState.done) {
+    //
+    //         return MaterialApp(
+    //           home: Scaffold(
+    //             appBar: AppBar(title: Text('Example')),
+    //             body: Column(
+    //               mainAxisAlignment: MainAxisAlignment.center,
+    //               children: [
+    //                 Text('\${watch(counterProvider).counter}'),
+    //                 RaisedButton(
+    //                   child: Text('add data'),
+    //                   color: Colors.orange,
+    //                   textColor: Colors.white,
+    //                   onPressed: addUser,
+    //                 )
+    //               ],
+    //             ),
+    //             floatingActionButton: _RiverpodPracticePageFab(),
+    //           ),
+    //         );
+    //       }
+    //
+    //       return Center(
+    //         child: CircularProgressIndicator(),
+    //       );
+    //     });
+
   }
 }
 
